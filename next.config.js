3401b09a86945dad213458bb10824e9165ab21a5
iSpeakNerd <96841384+iSpeakNerd@users.noreply.github.com>
2024-08-09 13:10:16 -0700

    make like tabletop

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-}
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/players',
+        destination: process.env.SPREADSHEET_URL, // inject URL from vercel
+        permanent: true,
+      },
+      {
+        source: '/channel',
+        destination: 'https://warpcast.com/~/channel/tabletop', // warpcast channel URL
+        permanent: true,
+      },
+      {
+        source: '/blog',
+        destination: 'https://paragraph.xyz/@ispeaknerd.eth/category/tabletop', // paragraph blog URL
+        permanent: true,
+      },
+      {
+        source: '/community-stats',
+        destination: process.env.PUBLIC_STATS_URL, // inject URL from vercel
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
