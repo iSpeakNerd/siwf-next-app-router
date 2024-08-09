417c3d64044987f2abc8312fe1ce88a7d992e17d
iSpeakNerd <96841384+iSpeakNerd@users.noreply.github.com>
2024-08-09 14:20:09 -0700

    ui

--- components/card.tsx
+++ components/card.tsx
@@ -9,17 +9,17 @@ export default function Card(props: {
     <>
       <Link
         href={props.url}
-        className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
+        className='max-w-8 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
         target='_blank'
         rel='noopener noreferrer'
       >
-        <h2 className='mb-3 text-2xl font-semibold'>
+        <h2 className='text-pretty mb-3 text-2xl font-semibold'>
           {props.title}{' '}
-          <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+          <span className='max-w-[30ch] inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
             -&gt;
           </span>
         </h2>
-        <p className='m-0 max-w-[30ch] text-sm opacity-50'>
+        <p className='m-0 text-center text-sm opacity-50 text-pretty md:text-balance lg:text-balance'>
           {props.description}
           {/* See Tabletop player stats and&nbsp;details. */}
         </p>
--- components/homepage.tsx
+++ components/homepage.tsx
@@ -5,38 +5,33 @@ export default function HomePage() {
   return (
     <>
       <SiteId />
-      <div className='mb-4 z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex md:flex'>
-        {/* Static link buttons */}
-        <div className='mb-6 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-5 lg:text-left'>
-          <Card
-            url='/players'
-            title='Players'
-            description='See Tabletop player stats and&nbsp;details.'
-          />
-
-          <Card
-            url='/blog'
-            title='Blog'
-            description='See the latest Tabletop news and updates.'
-          />
-          <Card
-            url='/channel'
-            title='Channel'
-            description='Check out the Tabletop channel on Farcaster.'
-          />
-
-          <Card
-            url='/community-stats'
-            title='Stats'
-            description='View the Tabletop community stats and&nbsp;insights.'
-          />
-
-          <Card
-            url='https://farcasteruserstats.com/channels/tabletop'
-            title='Channel Stats'
-            description='View the Tabletop channel stats and insights, provided by Farcaster Studio.'
-          />
-        </div>
+      {/* Static link buttons */}
+      <div className='mb-6 md:max-w-8 grid text-center w-full lg:mb-0 lg:w-full lg:grid-cols-5 lg:text-left'>
+        <Card
+          url='/players'
+          title='Players'
+          description='See Tabletop player stats and&nbsp;details.'
+        />
+        <Card
+          url='/blog'
+          title='Blog'
+          description='See the latest Tabletop news and&nbsp;updates.'
+        />
+        <Card
+          url='/channel'
+          title='Channel'
+          description='Check out the Tabletop channel on&nbsp;Farcaster.'
+        />
+        <Card
+          url='/community-stats'
+          title='Stats'
+          description='View the Tabletop community stats and&nbsp;insights.'
+        />
+        <Card
+          url='https://farcasteruserstats.com/channels/tabletop'
+          title='Channel Stats'
+          description='View the Tabletop channel stats and insights, provided by Farcaster&nbsp;Studio.'
+        />
       </div>
     </>
   );
